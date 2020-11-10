0f9e1679998c9ff990b16c18ef32336df61e8622
ErdosXenia <erdos.xenia@gmail.com>
2020-11-10 11:23:35 +0100

    5. Feladat es a maradek befejezese

--- Kutyak/Program.cs
+++ Kutyak/Program.cs
@@ -11,6 +11,8 @@ namespace Kutyak
     class Program
     {
         static List<KutyaNev> KutyaNevek = new List<KutyaNev>();
+        static List<KutyaFajta> KutyaFajtak = new List<KutyaFajta>();
+        static List<Kutya> KutyaLista = new List<Kutya>();
 
         static void KutyaNevekBeolvasas()
         {
@@ -36,16 +38,48 @@ namespace Kutyak
             Console.WriteLine($"3. Feladat: Kutyanevek száma: {KutyaNevek.Count()}");
         }
 
-        static void feladat4()
+        static void KutyaFajtakBeolvasas()
         {
+            StreamReader sr = new StreamReader("KutyaFajtak.csv");
 
+            sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                string[] adat = sr.ReadLine().Split(';');
+
+                KutyaFajtak.Add(new KutyaFajta(Convert.ToInt32(adat[0]), adat[1], adat[2]
+                ));
+            }
+
+            sr.Close();
+        }
+
+        static void KutyaBeolvasas()
+        {
+            StreamReader sr = new StreamReader("Kutyak.csv");
+
+            sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                string[] adat = sr.ReadLine().Split(';');
+
+                KutyaLista.Add(new Kutya(Convert.ToInt32(adat[0]), Convert.ToInt32(adat[1]), 
+                    Convert.ToInt32(adat[2]), Convert.ToInt32(adat[3]), adat[4]));
+            }
+
+            sr.Close();
         }
 
         static void Main(string[] args)
         {
             KutyaNevekBeolvasas();
             feladat3();
+            KutyaFajtakBeolvasas();
+            KutyaBeolvasas();
 
+           
             Console.ReadKey();
         }
     }
